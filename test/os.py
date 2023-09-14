386f6211fdc4f4271e60de9421760803fd3ee4a9
Dongju Yeom <ehdwn4659@naver.com>
2023-09-14 16:28:12 +0200

    second commit

--- test/os.py
+++ test/os.py
@@ -1,4 +1,9 @@
 import os
-os.getcwd()
-os.mkdir("test")
+
+current_directory = os.getcwd()
+print("Current directory:", current_directory)
+
+# Change the current working directory
+os.chdir("C:/Users/ehdwn/OneDrive/바탕 화면/python/test")
+# os.mkdir("test") #creating a folder
 
